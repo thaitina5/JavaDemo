0a64f342dda5ec5d723eb26df17f3c5d01b8cffd
thaitina5 <thaitina5@gmail.com>
2023-03-14 22:44:54 +0700

    JDBC_05(DAO),06(INSERT, UPDATE),07(SELECT)

--- JDBC_5/src/test/TestJDBCUtil06.java
+++ JDBC_5/src/test/TestJDBCUtil06.java
@@ -8,10 +8,10 @@ public class TestJDBCUtil06 {
 	public static void main(String[] args) {
 		/* Test Insert */
 		
-		Sach sach1 = new Sach("LTJV", "Lập trình Java", 50000, 2025);
-		Sach sach2 = new Sach("LTC", "Lập trình C", 70000, 2030);
-		SachDAO.getInstance().insert(sach1);
-		SachDAO.getInstance().insert(sach2);
+//		Sach sach1 = new Sach("LTJV", "Lập trình Java", 50000, 2025);
+//		Sach sach2 = new Sach("LTC", "Lập trình C", 70000, 2030);
+//		SachDAO.getInstance().insert(sach1);
+//		SachDAO.getInstance().insert(sach2);
 		
 		
 //		for (int i = 0; i < 1000; i++) {
@@ -22,8 +22,8 @@ public class TestJDBCUtil06 {
 		
 		
 		/* Test update */
-//		Sach sach2 = new Sach("LTC", "Lập trình C", 100000, 2030);
-//		SachDAO.getInstance().update(sach2);
+		Sach sach2 = new Sach("LTC", "Lập trình C", 100000, 2030);
+		SachDAO.getInstance().update(sach2);
 		
 		/* Test delete */
 //		for (int i = 0; i < 1000; i++) {
